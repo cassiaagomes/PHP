3d111ef5674b586ebfa4634f43089ee904898cd7
Cassia Gomes <128179609+cassiaagomes@users.noreply.github.com>
2024-03-19 22:30:01 -0300

    PHP

---
+++ blog_com_php/helpers/url.php
@@ -0,0 +1,8 @@
+<?php
+
+    //Informa onde é a raiz do sistema para linkar imagens e estilos
+    $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI'] . '?') . '/';
+
+
+
+?> 
